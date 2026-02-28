2c3357b8c105129bcf5f81bcb6d6d4704df09307
KeerthiReddy520 <keerthireddybandi.kr@gmail.com>
2026-02-28 14:44:07 +0530

    Add files via upload

---
+++ sum1.java
@@ -0,0 +1,10 @@
+public class sum1{
+    public static void main(String[] args){
+        int arr[] = {10, 20, 30};
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Sum= " + sum);
+    }
+}
